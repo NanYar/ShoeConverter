a580d6e9bd81ca008d76af8616a07ddcbcb73fd9
NanYar <github@sagner.org>
2014-10-09 22:13:42 +0200

    Finished tutorial project

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -12,41 +12,69 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mensShoeSizeTextField: UITextField!
     @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
-    
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+    @IBOutlet weak var womensConvertedShoeSizeLabel: UILabel!
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+
     
-    @IBAction func convertButtonPressed(sender: UIButton) {
+    @IBAction func mensConvertButtonPressed(sender: UIButton) {
         
-        let stringSizeFromTextField = mensShoeSizeTextField.text
-        let optionalNumberFromTextField = stringSizeFromTextField.toInt()
+//      let stringSizeFromTextField = mensShoeSizeTextField.text
+//      let optionalNumberFromTextField = stringSizeFromTextField.toInt()
+        
+        let optionalNumberFromTextField = mensShoeSizeTextField.text.toInt()
         
         if optionalNumberFromTextField != nil {
             let intFromTextField = optionalNumberFromTextField!
             
-            if intFromTextField > 2 && intFromTextField < 17 {
+            if intFromTextField > 1 && intFromTextField < 15 {
             
-            let conversionConstant = 33
-            let result = intFromTextField + conversionConstant
-            let stringWithUpdatedShoeSize = "\(result)"
+                let conversionConstant = 33
             
-            mensShoeSizeTextField.text = ""
-            mensShoeSizeTextField.resignFirstResponder()
-            mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize
-            mensConvertedShoeSizeLabel.hidden = false
+                mensShoeSizeTextField.text = ""
+                mensShoeSizeTextField.resignFirstResponder()
+                mensConvertedShoeSizeLabel.text = "= \(intFromTextField + conversionConstant) in EU shoe size"
+                mensConvertedShoeSizeLabel.hidden = false
+                
             } else {
                 mensShoeSizeTextField.text = ""
                 mensConvertedShoeSizeLabel.text = ""
             }
+            
+        } else {
+            mensShoeSizeTextField.text = ""
+            mensConvertedShoeSizeLabel.text = ""
         }
         
      }
+    
+    
+    @IBAction func womensConvertButtonPressed(sender: UIButton) {
+        
+        let doubleFromTextField = Double((womensShoeSizeTextField.text as NSString).doubleValue)
+        
+        if doubleFromTextField > 2 && doubleFromTextField < 16 {
+            
+            let conversionConstant = 31.5
+            
+            womensShoeSizeTextField.text = ""
+            womensShoeSizeTextField.resignFirstResponder()
+            womensConvertedShoeSizeLabel.text = "= \(doubleFromTextField + conversionConstant) in EU shoe size"
+            womensConvertedShoeSizeLabel.hidden = false
+            
+        } else {
+            womensShoeSizeTextField.text = ""
+            womensConvertedShoeSizeLabel.text = ""
+        }
+        
+    }
+    
 }
